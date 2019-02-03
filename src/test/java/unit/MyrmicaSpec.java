a10748cb5bcd78357c00da515256428858748361
Jesus Zazueta <jjzazuet@vaccovecrana.com>
2019-02-03 01:54:48 -0500

    - Updated test cases.

--- src/test/java/unit/MyrmicaSpec.java
+++ src/test/java/unit/MyrmicaSpec.java
@@ -27,8 +27,10 @@ public class MyrmicaSpec {
     String localRepo = "/tmp/repo/";
     Repository repo = new Repository(localRepo, M2);
 
+    Coordinates arrowJdbc = new Coordinates("org.apache.arrow", "arrow-jdbc", "0.12.0");
     Coordinates googleApiClient = new Coordinates("com.google.api-client", "google-api-client", "1.28.0");
     Coordinates hibernateCore = new Coordinates("org.hibernate", "hibernate-core", "5.4.1.Final");
+    Coordinates queryDslJpa = new Coordinates("com.querydsl", "querydsl-jpa", "4.2.1");
     Coordinates spark = new Coordinates("org.apache.spark", "spark-core_2.12", "2.4.0");
     Coordinates spring = new Coordinates("org.springframework.boot", "spring-boot-starter-web", "2.1.2.RELEASE");
     Coordinates opencv = new Coordinates("org.bytedeco.javacpp-presets", "opencv-platform", "4.0.1-1.4.4");
@@ -61,12 +63,16 @@ public class MyrmicaSpec {
     });
     it("Can install target runtime artifacts for large frameworks.", () -> {
 
+      ResolutionStats rsQueryDslJpa = ResolutionStats.installAndMatch(repo, queryDslJpa, "/com.querydsl^querydsl-jpa^4.2.1.grdl");
+      ResolutionStats rsArrowJdbc = ResolutionStats.installAndMatch(repo, arrowJdbc, "/org.apache.arrow^arrow-jdbc^0.12.0.grdl");
       ResolutionStats rsGoogleApiClient = ResolutionStats.installAndMatch(repo, googleApiClient, "/com.google.api-client^google-api-client^1.28.0.grdl");
       ResolutionStats rsHibernateCore = ResolutionStats.installAndMatch(repo, hibernateCore, "/org.hibernate^hibernate-core^5.4.1.Final.grdl");
       ResolutionStats rsSpring = ResolutionStats.installAndMatch(repo, spring, "/org.springframework.boot^spring-boot-starter-web^2.1.2.RELEASE.grdl");
       ResolutionStats rsSpark = ResolutionStats.installAndMatch(repo, spark, "/org.apache.spark^spark-core_2.12^2.4.0.grdl");
       ResolutionStats rsDl4j = ResolutionStats.installAndMatch(repo, dl4j, "/org.deeplearning4j^deeplearning4j-core^1.0.0-beta3.grdl");
 
+      log.info(rsQueryDslJpa.toString());
+      log.info(rsArrowJdbc.toString());
       log.info(rsGoogleApiClient.toString());
       log.info(rsHibernateCore.toString());
       log.info(rsSpring.toString());
